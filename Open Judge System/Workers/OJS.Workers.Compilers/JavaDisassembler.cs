849a62359cabe6d802bd2e76ee23d8587729db80
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-08-11 00:02:15 +0300

    Implemented JavaDisassembler class.

---
+++ Open Judge System/Workers/OJS.Workers.Compilers/JavaDisassembler.cs
@@ -0,0 +1,23 @@
+﻿namespace OJS.Workers.Compilers
+{
+    using System.Text;
+
+    public class JavaDisassembler : Compiler
+    {
+        public override string BuildCompilerArguments(string inputFile, string outputFile, string additionalArguments)
+        {
+            var arguments = new StringBuilder();
+
+            arguments.Append(additionalArguments);
+
+            arguments.AppendFormat(" {0} > {1}", inputFile, outputFile);
+
+            return arguments.ToString();
+        }
+
+        public override string GetOutputFileName(string inputFileName)
+        {
+            return inputFileName + ".txt";
+        }
+    }
+}
